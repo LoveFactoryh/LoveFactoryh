f704f8c5d4eba20beb87d9138b0c894d2f81de65
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-03-07 12:25:54 +0100

    probe things

--- src/main/java/com/example/LoveFactoryh/LoveFactoryhCrudApplication.java
+++ src/main/java/com/example/LoveFactoryh/LoveFactoryhCrudApplication.java
@@ -1,17 +1,23 @@
 package com.example.LoveFactoryh;
 
-import Controller.CController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
-
+@RequestMapping
 public class LoveFactoryhCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoveFactoryhCrudApplication.class, args);
-
-		CController controller = new CController();
-		controller.controller();
+		SpringApplication.run(LoveFactoryhCrudApplication.class, args);}
+	@GetMapping("/hola")
+	public String controller(){
+		//model.addAttribute("saludo", "Mi primera aplicacion web Spring Boot");
+		return "Funciona de una puta vez";
 	}
-}
+
+	}
+
