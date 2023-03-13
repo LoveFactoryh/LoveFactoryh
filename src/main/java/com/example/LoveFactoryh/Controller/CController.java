b2dc2ee9b0c164421806c3b2a206f22bea31290f
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-03-13 09:35:33 +0100

    th or not th this is the cuestion

--- src/main/java/com/example/LoveFactoryh/Controller/CController.java
+++ src/main/java/com/example/LoveFactoryh/Controller/CController.java
@@ -8,26 +8,27 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 
 @Controller
-@RequestMapping
+
 public class CController {
 @Autowired
 
     private IConsultation iConsultation;
 
 //    public  List<CConsultation> list(){
-//        return (List<CConsultation>) iConsultation.findAll();
+//        return (List<CConsultation>)iConsultation.findAll();
 //    }
 
 @GetMapping("/")
     public String list (Model model){
-       List<CConsultation> consultations = (List<CConsultation>)iConsultation.findAll();
-       model.addAttribute("consultation", consultations);
+       //List<CConsultation> consultations = (List<CConsultation>)iConsultation.findAll();
+       model.addAttribute("consultation", iConsultation.findAll());
        return "index";
     }
     @GetMapping("/form")
@@ -35,10 +36,10 @@ public class CController {
     model.addAttribute("consulting", new CConsultation());
     return "form";
     }
-
+    @PostMapping("/save")
    public String save(CConsultation cons, Model model){
     iConsultation.save(cons);
-    return "redirect:/listar";
+    return "redirect:/";
     }
 }
 
