5b2631c003570e5eabfeb5a7e6c4034b5fdfd073
celiacueria <celiacueria1999@gmail.com>
2023-03-16 14:50:00 +0100

    Security

--- src/main/java/com/example/LoveFactoryh/Config/WebSecurityConfig.java
+++ src/main/java/com/example/LoveFactoryh/Config/WebSecurityConfig.java
@@ -1,17 +1,29 @@
 package com.example.LoveFactoryh.Config;
 
+import com.example.LoveFactoryh.utils.EAuthorityName;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
+    //@Bean
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws  Exception{
+        return http
+                .httpBasic()
+                .and().authorizeHttpRequests()
+                .requestMatchers("/form").hasRole(EAuthorityName.ADMIN.name())
+                .and().build();
+    }
 //    @Bean
 //    public UserDetailsService userDetailsService() {
 //
